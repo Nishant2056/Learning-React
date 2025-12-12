f629e76b8bf0f331e2a3cb9e33287c66da089626
nishant bhurtel <bhurtelnishant@gmail.com>
2025-12-12 11:21:46 +0545

    dynamically added the new section by managing state

--- todo-dynamic/todo-list-dynamic-way/src/App.jsx
+++ todo-dynamic/todo-list-dynamic-way/src/App.jsx
@@ -1,21 +1,30 @@
 import InputPart from "./components/InputPart";
 import TodoItems from "./components/TodoItems";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  let requiredData = [
-    { task: "Go to market", date: "2024-06-10", priority: "High" },
-    { task: "Buy Groceries", date: "2024-06-11", priority: "Medium" },
-    { task: "Complete React Project", date: "2024-06-12", priority: "High" },
-    { task: "Read a book", date: "2024-06-13", priority: "Low" },
-    { task: "Watch a movie", date: "2024-06-14", priority: "Low" },
-    { task: "Call a friend", date: "2024-06-15", priority: "Medium" },
-    { task: "Attend a meeting", date: "2024-06-16", priority: "High" },
+  let initialRequiredData = [
+    { task: "Go to market", date: "2024-06-10" },
+    { task: "Buy Groceries", date: "2024-06-11" },
+    { task: "Complete React Project", date: "2024-06-12" },
+    { task: "Read a book", date: "2024-06-13" },
+    { task: "Watch a movie", date: "2024-06-14" },
+    { task: "Call a friend", date: "2024-06-15" },
+    { task: "Attend a meeting", date: "2024-06-16" },
   ];
+
+  const [requiredData, setRequiredData] = useState(initialRequiredData);
+
+  const handleNewItem = (task, date) => {
+    const newAddedItem = [...requiredData, { task, date }];
+    setRequiredData(newAddedItem);
+  };
+
   return (
     <center>
       <h1 className="heading">Todo-List</h1>
-      <InputPart></InputPart>
+      <InputPart onNewItem={handleNewItem}></InputPart>
       <TodoItems todoItems={requiredData}></TodoItems>
     </center>
   );
--- todo-dynamic/todo-list-dynamic-way/src/components/TodoUi.jsx
+++ todo-dynamic/todo-list-dynamic-way/src/components/TodoUi.jsx
@@ -1,10 +1,9 @@
-const TodoUi = ({ task, date, priority }) => {
+const TodoUi = ({ task, date }) => {
   return (
     <div className="container">
       <div className="row kg-row">
         <div className="col-4">{task}</div>
         <div className="col-4">{date}</div>
-        <div className="col-1">pr: {priority}</div>
         <div className="col-1">
           <button type="button" className="btn btn-danger kg-button">
             Delete
